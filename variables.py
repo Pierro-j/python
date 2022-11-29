a5cb9837b92e677f2877538f4641bf3434b5efc9
pierre <jarzynkapierre07@gmail.com>
2022-11-29 11:46:37 +0100

    Ajout variables
    Création fichier
    
    Ajout variables

---
+++ variables.py
@@ -0,0 +1,12 @@
+# l'operateur d'affectation = permet d'injecter une valeur a une variable
+
+
+my_number1 = 123
+my_number2 = -42
+
+
+my_text = 'chaine de caractères'
+my_text2 = 'Autre chaine de caractères'
+
+print (my_text + ' ' + my_text2)
+print (my_number1 + my_number2)
