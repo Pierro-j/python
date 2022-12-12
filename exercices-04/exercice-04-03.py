e27dfccc8f1a24af06b2a85c2338203b1936726d
pierre <jarzynkapierre07@gmail.com>
2022-12-12 16:13:24 +0100

    Exo 4 fini

--- exercices-04/exercice-04-03.py
+++ exercices-04/exercice-04-03.py
@@ -15,3 +15,10 @@ if number >= 3:
 else:
     print("le nombre n'est pas divisible par 3")
 
+# Ou
+
+if number % 3 ==0 :
+    print("le nombre est divisible par 3")
+else:
+    print("le nombre n'est pas divisible par 3")
+
